71324c2e5a3e44f6019b7d3e7b316a7d660176c5
JoaoVitor <joaovitormsilva1@gmail.com>
2022-05-11 17:17:45 -0300

    Realizacao dos exercicios

--- Variaveis.cpp
+++ Variaveis.cpp
@@ -4,6 +4,9 @@
 int main(){
     int NumComputadores=100, SerialComputadores=11111;
     std::cout<<"$$$$$$$$$$$$$$$$"<<std::endl;
-    
+    std::cout<<"NumComputadores = "<<NumComputadores<<std::endl;
+    std::cout<<"valor de SerialComputadores: "<<SerialComputadores<<std::endl;
+    std::cout<<"$$$$$$$$$$$$$$$$"<<std::endl;
+ 
     return 0;
 }
